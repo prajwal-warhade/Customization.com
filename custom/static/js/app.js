2b89eb2627cde212ab31afa47db49d9b513ac96f
Prajwal Warhade <144923446+prajwal-warhade@users.noreply.github.com>
2024-03-27 20:50:18 +0530

    Fix line endings and added Navbar also css file now working Fevicon are added by prajwal

--- custom/static/js/app.js
+++ custom/static/js/app.js
@@ -1,17 +1,17 @@
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
-const container = document.querySelector(".container");
+const container1 = document.querySelector(".container1");
 const sign_in_btn2 = document.querySelector("#sign-in-btn2");
 const sign_up_btn2 = document.querySelector("#sign-up-btn2");
 sign_up_btn.addEventListener("click", () => {
-    container.classList.add("sign-up-mode");
+    container1.classList.add("sign-up-mode");
 });
 sign_in_btn.addEventListener("click", () => {
-    container.classList.remove("sign-up-mode");
+    container1.classList.remove("sign-up-mode");
 });
 sign_up_btn2.addEventListener("click", () => {
-    container.classList.add("sign-up-mode2");
+    container1.classList.add("sign-up-mode2");
 });
 sign_in_btn2.addEventListener("click", () => {
-    container.classList.remove("sign-up-mode2");
+    container1.classList.remove("sign-up-mode2");
 });
